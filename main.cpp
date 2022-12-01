2389c03c9d03300bf66df9ab38e7d0561fe5c5dd
Halil Akca <halil.akca45@myhunter.cuny.edu>
2022-12-01 05:57:05 -0500

    created file

---
+++ main.cpp
@@ -0,0 +1,17 @@
+/*
+  Author: Halil Akca
+  Course: CSCI-135
+  Instructor: Mike Zamansky
+  Assignment: Lab 12
+  This program will call the functions made in other .cpp files
+*/
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include "funcs.h"
+
+int main()
+{
+    return 0;
+}
