4f05398940401a8aece60c90b1a56610713526c7
Halil Akca <halil.akca45@myhunter.cuny.edu>
2022-12-01 06:37:08 -0500

    added missing function

--- funcs.h
+++ funcs.h
@@ -1,5 +1,6 @@
 #pragma once
 
+void printVector(std::vector<int> &test);
 std::vector<int> makeVector(int n);
 std::vector<int> goodVibes(const std::vector<int> &v);
 void gogeta(std::vector<int> &goku, std::vector<int> &vegeta);
