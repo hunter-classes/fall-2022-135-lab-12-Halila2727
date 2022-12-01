9e87c8923ef3a69b828f81027caf49d6b9abd6fb
Halil Akca <halil.akca45@myhunter.cuny.edu>
2022-12-01 05:56:16 -0500

    added all function headers

---
+++ funcs.cpp
@@ -0,0 +1,31 @@
+/*
+    Author: Halil Akca
+    Course: CSCI-135
+    Instructor: Zamansky
+    Assignment: Lab 12
+    This program will perform all the tasks
+*/
+
+#include <iostream>
+#include <cmath>
+#include <vector>
+
+std::vector<int> makeVector(int n)
+{
+
+}
+
+std::vector<int> goodVibes(const std::vector<int> &v)
+{
+
+}
+
+void gogeta(std::vector<int> &goku, std::vector<int> &vegeta)
+{
+
+}
+
+std::vector<int> sumPairWise(const std::vector<int> &v1, const std::vector<int> &v2)
+{
+
+}
